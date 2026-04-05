89c458026b0cf74c00e2222cfcc53c364f0df365
杨明明 <13707931+yangnetwork@user.noreply.gitee.com>
2026-04-06 00:02:44 +0800

    feat: center nav and add coming soon toast

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const showToast = ref(false)
+let toastTimer: number | undefined
+
+function showComingSoon() {
+  showToast.value = true
+
+  if (toastTimer) {
+    window.clearTimeout(toastTimer)
+  }
+
+  toastTimer = window.setTimeout(() => {
+    showToast.value = false
+  }, 1800)
+}
+</script>
+
 <template>
   <header class="topbar">
     <div class="topbar-inner">
@@ -8,6 +27,9 @@
 
       <nav class="topnav" aria-label="Primary">
         <RouterLink class="topnav-link" active-class="is-active" to="/">汉字词游戏</RouterLink>
+        <button class="topnav-link topnav-link--button" type="button" @click="showComingSoon">
+          音读关系图谱
+        </button>
         <RouterLink class="topnav-link" active-class="is-active" to="/joyo-kanji">2136 当用汉字</RouterLink>
         <a class="topnav-link"
           href="https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIyMTAyMzMwMw==&action=getalbum&album_id=3021357860343234566&subscene=126&scenenote=https%3A%2F%2Fmp.weixin.qq.com%2Fs%3F__biz%3DMzIyMTAyMzMwMw%3D%3D%26mid%3D2647702160%26idx%3D1%26sn%3D9402d096d6239003c93b8c0d60beaf36%26chksm%3D8e168194c5c0b6761381c005533bedcfb1c4bee03d02f267960e7e97692ceb180c12284fc84c%26scene%3D126%26sessionid%3D1775323067%26subscene%3D227%26clicktime%3D1775323072%26enterid%3D1775323072%26click_id%3D76%26key%3Ddaf9bdc5abc4e8d0f1e077786dd7f033f4143094c1b97f01d6d5c17f314334861985dff8d3ff0c85b5daa58bf3eb01d23f55ead68cc8b7a9a2dcab56bf6d6177430680ea30fe374a70d1150e7ee21387b1ebb5b753907f9ab46452390c78a9ca20ef35a19c6a95a387d2537365f6c50b0b9c5546ac3b1e274810acdf6dce4802%26ascene%3D1%26uin%3DMjQzNTIzNjc4MQ%253D%253D%26devicetype%3DUnifiedPCMac%26version%3Df2641210%26lang%3Dzh_CN%26countrycode%3DCN%26exportkey%3Dn_ChQIAhIQMYntZ4b9208UO7wGun9kqxLkAQIE97dBBAEAAAAAAF2DLduYRPkAAAAOpnltbLcz9gKNyK89dVj0vG1Ne8JNIk68%252BYhtA2SjpG5Y0ElI2cleTNEkHU7IDuyMvFIb0vsAwcGEK%252FrhHeADutnxj%252BDCQ%252BvWtgnfMQgXt5DzfqQrw3q7lLlgWHjYvETM3NCXAx5SMYxi2EP1%252Bz6PjL1m7vHQA8sGCZ%252FgN5DNVpj7WKUlZQ56gKjHqXMstANobc6EaRnP5vcgZkK6XQt4ljAtdv2pdIBdyqBc7w3gk4MUf%252FF1YaI0hYIfysrM%252FdewDyqOhUNxSeced3xpYA%253D%253D%26acctmode%3D0%26pass_ticket%3D%252FhcvN3lzg1gBlAd4dz%252BA6fZ%252BBgPiU9nPBK%252FZzpyo0miMumKwjnPlPGjyxKin4ODp%26wx_header%3D0%26fasttmpl_type%3D0%26fasttmpl_fullversion%3D8198749-zh_CN-html%26from_xworker%3D1&nolastread=1&sessionid=#wechat_redirect"
@@ -18,5 +40,11 @@
         <RouterLink class="topnav-link" active-class="is-active" to="/about">关于我</RouterLink>
       </nav>
     </div>
+
+    <Transition name="site-toast">
+      <div v-if="showToast" class="site-toast" role="status" aria-live="polite">
+        音读关系图谱，敬请期待
+      </div>
+    </Transition>
   </header>
 </template>
